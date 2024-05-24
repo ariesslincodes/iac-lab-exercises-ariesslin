32323a27bdeecfba4451de882d17a728cca7ee1e
Ariess Lin <ariess.lin@thoughtworks.com>
2024-05-24 23:54:39 +0200

    [session2] Add route tables

--- Session_2_Terraform_Basics/network.tf
+++ Session_2_Terraform_Basics/network.tf
@@ -75,7 +75,7 @@ resource "aws_subnet" "secure_subnet_2" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
   tags = {
@@ -84,10 +84,10 @@ resource "aws_internet_gateway" "gw" {
 }
 
 resource "aws_eip" "ngw_for_subnet_1" {
-  domain   = "vpc"
+  domain = "vpc"
 
   tags = {
-    Name = format("%s-lb", var.prefix)
+    Name = format("%s-eip", var.prefix)
   }
 }
 
@@ -97,5 +97,31 @@ resource "aws_nat_gateway" "ngw" {
 
   tags = {
     Name = format("%s-ngw", var.prefix)
+  }
+}
+
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = format("%s-route-table-for-public-subnet", var.prefix)
+  }
+}
+
+resource "aws_route_table" "private_route" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    ipv6_cidr_block = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.ngw.id
+  }
+
+  tags = {
+    Name = format("%s-route-table-for-private-subnet", var.prefix)
   }
 }
