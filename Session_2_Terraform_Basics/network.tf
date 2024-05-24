830c8cbac4fe2b6c216a3028caaf2ff12b6abd73
Ariess Lin <ariess.lin@thoughtworks.com>
2024-05-24 23:58:46 +0200

    [session2] Add route table associations

--- Session_2_Terraform_Basics/network.tf
+++ Session_2_Terraform_Basics/network.tf
@@ -125,3 +125,23 @@ resource "aws_route_table" "private_route" {
     Name = format("%s-route-table-for-private-subnet", var.prefix)
   }
 }
+
+resource "aws_route_table_association" "public_subnet_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+resource "aws_route_table_association" "public_subnet_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+resource "aws_route_table_association" "private_subnet_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route.id
+}
+
+resource "aws_route_table_association" "private_subnet_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route.id
+}
