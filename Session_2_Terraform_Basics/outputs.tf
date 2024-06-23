df5cbe44f63be88e7767ab1e7dc7c6d7eaab7792
Ariess Lin <ariess.lin@thoughtworks.com>
2024-06-23 16:55:14 +0200

    [session2] Update output

--- Session_2_Terraform_Basics/outputs.tf
+++ Session_2_Terraform_Basics/outputs.tf
@@ -2,3 +2,8 @@ output "nat_eip_ip_addr" {
   value       = aws_eip.nat.public_ip
   description = "The elastic IP address for NAT gateway."
 }
+
+output "vpc_id" {
+  value       = aws_vpc.vpc.id
+  description = "The ID of the VPC."
+}
