17ab8dde0c7a667db2d992205c16e76ac3023b34
Ariess Lin <ariess.lin@thoughtworks.com>
2024-06-24 23:32:27 +0200

    [session5] Add backend support

---
+++ Session_5_Terraform_Modules/backend_support/s3.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "${var.prefix}-tfstate"
+  force_destroy = true
+  lifecycle { prevent_destroy = true }
+
+  tags = {
+    Name        = "${var.prefix}-tfstate"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
